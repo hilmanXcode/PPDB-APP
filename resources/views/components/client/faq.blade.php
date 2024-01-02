b7b6dc15ec29a4c58deb9ff7e4a05a838c990791
Craftee <yakali345@gmail.com>
2024-01-02 10:52:22 +0700

    improved faq display and added fadeinup animation

--- resources/views/components/client/faq.blade.php
+++ resources/views/components/client/faq.blade.php
@@ -1,4 +1,4 @@
-    <!-- FAQ Start  -->
+   <!-- FAQ Start
     <div class="container-xxl py-5 category">
         <div class="container">
             <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
@@ -87,4 +87,122 @@
             </div>
         </div>
     </div>
-    <!-- FAQ End  -->
+    FAQ End -->
+
+   <!-- ======= F.A.Q Section ======= -->
+   <div class="container-xxl py-5 category">
+       <div class="container">
+           <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+
+               <h1 class="mb-5">Frequently Asked Questions</h1>
+           </div>
+
+           <div class="row g-2">
+               <div class="col-lg-6">
+                   <!-- F.A.Q List 1-->
+                   <div class="accordion accordion-flush" id="faqlist1">
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
+                                   Bagaimana cara mendaftar?
+                               </button>
+                           </h2>
+                           <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+                               <div class="accordion-body">
+                                   Caranya cukup mudah dengan cara klik tombol daftar sekarang yang ada di pojok kanan
+                                   atas atau klik <a class="scrollto" href="#daftar">disini<i
+                                           class="ri-arrow-right-up-line"></i></a>
+                               </div>
+                           </div>
+                       </div>
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
+                                   Apakah daftarnya wajib online?
+                               </button>
+                           </h2>
+                           <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+                               <div class="accordion-body">
+                                   Wajib online, tapi boleh offline bagi yang berkenan datang langsung ke sekolah
+
+                               </div>
+                           </div>
+                       </div>
+
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
+                                   Apakah tesnya boleh online?
+                               </button>
+                           </h2>
+                           <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist1">
+                               <div class="accordion-body">
+                                   Untuk tes masuk wajib datang langsung ke sekolah
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+
+               <div class="col-lg-6">
+                   <!-- F.A.Q List 2-->
+                   <div class="accordion accordion-flush" id="faqlist2">
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-1">
+                                   dimana lokasi SMK PGRI Telagasari?
+                               </button>
+                           </h2>
+                           <div id="faq2-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+                               <div class="accordion-body">
+                                   Jl. Raya Syeh Quro, Talagasari, Kabupaten Karawang, Jawa Barat 41381, Indonesia
+                               </div>
+                           </div>
+                       </div>
+
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-2">
+                                   Apa saja fasilitas yang disediakan?
+                               </button>
+                           </h2>
+                           <div id="faq2-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+                               <div class="accordion-body">
+                                   Untuk informasi mengenai fasilitas
+                                   dapat dilihat
+                                   <a href="https://alfurqanpaser.com/fasilitas" target="_blank">disini<i
+                                           class="ri-arrow-right-up-line"></i></a>
+                               </div>
+                           </div>
+                       </div>
+
+                       <div class="accordion-item">
+                           <h2 class="accordion-header">
+                               <button class="accordion-button collapsed wow fadeInUp" data-wow-delay="0.3s"
+                                   type="button" data-bs-toggle="collapse" data-bs-target="#faq2-content-3">
+                                   Apa saja jurusan yang tersedia di sekoalh ini?
+                               </button>
+                           </h2>
+                           <div id="faq2-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist2">
+                               <div class="accordion-body">
+                                   <p></p>
+                                   <ul>
+                                       <li>Rekayasa Perangkat Lunak</li>
+                                       <li>Teknik Mesin Industri</li>
+                                       <li>Teknik Kendaraan Ringan</li>
+                                       <li>Teknik Pengelasan</li>
+                                   </ul>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       </section>
+       <!-- End F.A.Q Section -->
