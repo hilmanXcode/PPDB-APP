68e1fbbc1b0cc864c6fd6143d1f73ef068c85a71
harioblackid <50513039+harioblackid@users.noreply.github.com>
2024-01-04 14:03:42 +0700

    Add favicon

--- resources/views/components/dashboard/header.blade.php
+++ resources/views/components/dashboard/header.blade.php
@@ -5,7 +5,10 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin Dashboard</title>
-  {{-- <link rel="shortcut icon" type="image/png" href="{{ asset("images") }}" /> --}}
+
+  <!-- Favicon -->
+  <link rel="icon" href="{{asset('img/icon/favicon.ico')}}" type="img/icon">
+
   <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
