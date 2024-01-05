87daf0db17b3cc0af6d03c5cd1a5aca80b889c85
Craftee <yakali345@gmail.com>
2024-01-05 15:53:36 +0700

    update testimonial.blade.php

--- resources/views/components/client/testimonial.blade.php
+++ resources/views/components/client/testimonial.blade.php
@@ -1,9 +1,9 @@
 <div class="container-xxl py-5">
-    <div class="container">
+    <div class="container" data-aos="fade-up">
         <div class="text-center mx-auto" style="max-width: 500px">
             <h1 class="display-6 mb-5">Kata Alumni</h1>
         </div>
-        <div class="row g-5">
+        <div class="testimonials-slider swiper row g-5" data-aos="fade-up" data-aos-delay="200">
             <div class="col-lg-3 d-none d-lg-block">
                 <div class="testimonial-left h-100">
                 </div>
@@ -11,47 +11,31 @@
             <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
                 <div class="owl-carousel testimonial-carousel">
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/ardi.jpg')}}" alt="" />
+                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/fahmi.jpg')}}" alt="" />
                         <p class="fs-5">
-                            SMK SETARA DENGAN D3 PENGAJARANNYA.
+                            Jangan pernah takut untuk selalu mencoba hal baru, dan jadilah yang terbaik
+                            diantara yang lain
                         </p>
-                        <h5>Ardiansyah Em Habib</h5>
-                        <span>IT Desain Grafis</span>
+                        <h5>Fahmi Andika Setiono</h5>
+                        <span>Mahasiswa</span>
                     </div>
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/rizky.jpg')}}" alt="" />
+                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/amelia.jpg')}}" alt="" />
                         <p class="fs-5">
                             Sekolah sejuta kenangan, dengan banyak cerita menarik dan ilmu yang bisa didapatkan.
                         </p>
-                        <h5>Rizky Musthofa</h5>
-                        <span>IT Programmer</span>
+                        <h5>Amelia</h5>
+                        <span></span>
                     </div>
                     <div class="testimonial-item text-center">
-                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/ari.jpeg')}}" alt="" />
+                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/fitra.jpg')}}" alt="" />
                         <p class="fs-5">
-                            Sekolah yang nyaman dengan guru guru yang sangat asik, membuat belajar menjadi lebih betah!
+                            Saya bukan pesulap, saya hanyalah seorang frontend developer. Tapi, dengan beberapa baris
+                            kode, saya bisa
+                            bikin sesuatu muncul dan menghilang.
                         </p>
-                        <h5>Ari Farhan</h5>
-                        <span>IT Programmer</span>
-                    </div>
-                    <div class="testimonial-item text-center">
-                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/siska.jpg')}}" alt="" />
-                        <p class="fs-5">
-                            Dengan guru guru yang sudah tersertifikasi, membuat belajar komputer semakin lebih terarah.
-                            Yuk jadi bagian Telematika.
-                        </p>
-                        <h5>Siska Komalasari</h5>
-                        <span>Staff Administrator</span>
-                    </div>
-                    <div class="testimonial-item text-center">
-                        <img class="img-fluid rounded mx-auto mb-4" src="{{asset('default/eci.jpg')}}" alt="" />
-                        <p class="fs-5">
-                            Terimakasih banyak almamater ku telah banyak memberikan pelajaran dan pengajaran tentang
-                            dunia IT, yang dulunya sangat buta akan teknologi kini sedikit membuka mata tentang IT lewat
-                            SMK PGRI Telagasari
-                        </p>
-                        <h5>Sinengsih</h5>
-                        <span>Mahasiswi AMIK Purnama Niaga</span>
+                        <h5>Fitra Muhammad Anugrah</h5>
+                        <span>Frontend Developer</span>
                     </div>
                 </div>
             </div>
