3ed33b447687e8220dea5f024062b0f3a644b9c9
Craftee <yakali345@gmail.com>
2024-01-09 10:51:52 +0700

    Enhance service.blade.php and change informasi to blog

--- resources/views/Client/informasi.blade.php
+++ resources/views/Client/informasi.blade.php
@@ -1,27 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-    <x-client.head />
-  <body>
+<x-client.head />
+
+<body>
     <!-- Spinner Start -->
-   <x-client.spinner />
+    <x-client.spinner />
     <!-- Spinner End -->
 
 
     <!-- Navbar Start -->
     <x-client.navbar />
     <!-- Navbar End -->
-    
+
     <!-- Service Start -->
-   <x-client.service :info="$info"/>
+    <x-client.service :info="$info" />
     <!-- Service End -->
 
     <!-- Footer Start -->
-   <x-client.footer />
+    <x-client.footer />
     <!-- Footer End -->
 
     <!-- Back to Top -->
     <x-client.backtotop />
 
     <x-client.script />
-  </body>
+</body>
+
 </html>
--- resources/views/components/client/service.blade.php
+++ resources/views/components/client/service.blade.php
@@ -1,158 +1,139 @@
-<div class="container-xxl py-5">
-    <div class="container">
-        <div class="row g-4 justify-content-center">
-            @forelse($info as $infor)
-            <!-- -----------x---------- Site Content -------------x------------>
-            <section id="blog">
-                <!-- HEADING -->
-                <div class="blog-heading">
-                    <span>Recent Post</span>
-                    <h3>All Blogs</h3>
+<div class="container mt-5">
+    <div class="row">
+        <!-- -----------x---------- LOOP -------------x------------>
+        @forelse ($info as $infor)
+        <div class="col-lg-3 col-md-6">
+            <div class="card">
+                <div class="blog-image">
+                    <img src="images/image4.jpg">
+                    <div class="tag bg-primary">blog</div>
                 </div>
-                <!-- BLOG CONTENT -->
-                <div class="blog-container">
-                    <!-- BOX 1-->
-                    <div class="blog-box">
-                        <!-- IMG -->
-                        <div class="blog-img">
-                            <img src="{{asset('img/education.jpg')}}" alt="">
+                <div class="blog-content">
+                    <div class="blog-title">{{$infor->judul}}</div>
+                    <div class="blog-subtitle">SMK PGRI Telagasari</div>
+                    <p class="blog-desc">{{Str::limit($infor->deskripsi_informasi, 100, '.....')}}<a
+                            href="{{route('baca', $infor->id)}}">Read
+                            More</a></p>
+                    <div class="blog-footer">
+                        <div class="blog-avatar"><img src="{{asset('img/user.png')}}"></div>
+                        <div>
+                            <span class="blog-author">Administrator</span>
+                            <div class="blog-post">
+                                <span><i class="fa fa-clock-o"></i> 6 days ago</span>
+                                <span class="blog-comment"><i class="fa fa-comments"></i> 0 comments</span>
+                            </div>
                         </div>
-                        <!-- DESC -->
-                        <div class="blog-text">
-                            <span>07 January 2024</span>
-                            <a href="#" class="blog-title">{{$infor->judul}}</a>
-                            <p> {{Str::limit($infor->deskripsi_informasi, 100, '.....')}}</p>
-                            <a href="{{route('baca', $infor->id)}}">Read More</a>
-                        </div>
+                        <button class="btn btn-sm"><i class="fa fa-share-alt"></i></button>
                     </div>
                 </div>
-
-            </section>
-            <!-- -----------x---------- Site Content -------------x------------>
-            @empty
-            <div class="d-flex justify-content-center">Tidak Ada Informasi Apapun</div>
-            @endforelse
+            </div>
         </div>
+        @empty
+        <div class="d-flex justify-content-center">Tidak Ada Informasi Apapun</div>
+
+        @endforelse
+        <!-- -----------x---------- END LOOP -------------x------------>
     </div>
 </div>
+</body>
+
+</html>
 
 <style>
-#blog {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    padding: 40px;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+.blog-image {
+    position: relative;
 }
 
-.blog-heading {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-}
-
-.blog-heading span {
-    color: #015fc9;
-}
-
-.blog-heading h3 {
-    font-size: 2.4rem;
-    font-weight: 600;
-    color: #2b2b2b;
-}
-
-.blog-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 20px 0px;
-    flex-wrap: wrap;
-}
-
-.blog-box {
-    width: 350px;
-    background-color: white;
-    border: 1px solid #ececec;
-    margin: 20px;
-}
-
-.blog-img {
+.blog-image img {
     width: 100%;
-    height: auto;
 }
 
-.blog-img img {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    object-position: center;
-}
-
-.blog-text {
-    padding: 30px;
+.date {
+    width: 39px;
+    height: 39px;
+    border-radius: 50%;
+    position: absolute;
+    top: 3%;
+    right: 3%;
     display: flex;
-    flex-direction: column;
-}
-
-.blog-text span {
-    color: #015fc9;
-    font-size: 0.9rem;
-}
-
-.blog-text .blog-title {
-    font-size: 1.3rem;
+    align-items: center;
+    text-align: center;
+    color: #ffffff;
+    font-size: 13px;
+    line-height: 1;
     font-weight: 500;
-    color: #272727;
-
 }
 
-.blog-text .blog-title:hover {
-    color: #015fc9;
-    transition: all ease 0.3s;
+.tag {
+    position: absolute;
+    bottom: 0;
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 500;
+    padding: 0 15px;
+    color: #ffffff;
 }
 
-.blog-text p {
-    color: #9b9b9b;
-    font-size: 0.9rem;
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
+.blog-content {
+    padding: 25px 15px;
+}
+
+.blog-title {
+    font-size: 18px;
+    font-weight: 500;
+}
+
+.blog-subtitle {
+    font-weight: 400;
+    color: #ff003b;
+    margin-bottom: 16px;
+}
+
+.blog-desc {
+    font-size: 14px;
+}
+
+.blog-desc a {
+    text-decoration: none;
+    color: #ff003b;
+    font-weight: 500;
+}
+
+.blog-footer {
+    display: flex;
+    align-items: center;
+    margin-top: 35px;
+}
+
+.blog-author {
+    font-size: 14px;
+    font-weight: 500;
+}
+
+.blog-post {
+    font-size: 12px;
+    color: #b3b2b2;
+}
+
+.blog-avatar {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    display: block;
     overflow: hidden;
-    text-overflow: ellipsis;
-    margin: 20px 0px;
+    margin-right: 20px;
 }
 
-.blog-text a {
-    color: #0f0f0f;
+.blog-avatar img {
+    width: 100%;
 }
 
-.blog-text a:hover {
-    color: #015fc9;
-    transition: all ease 0.3s;
+.blog-comment {
+    margin-left: 10px;
 }
 
-@media(max-width:1250px) {
-    .blog-box {
-        width: 300px;
-    }
-}
-
-@media(max-width:1100px) {
-    .blog-box {
-        width: 70%;
-    }
-}
-
-@media(max-width:550px) {
-    .blog-box {
-        width: 100%;
-        margin: 20px 10px;
-    }
-
-    #blog {
-        padding: 20px;
-    }
+.blog-footer .btn {
+    margin-left: auto;
 }
 </style>
