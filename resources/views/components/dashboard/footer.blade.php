aa58b3d0693f832423d6c99ea6b032974f715b6f
Hilman.js <62336112+hilmanXcode@users.noreply.github.com>
2024-01-12 14:16:53 +0700

    Fix bug on login page

--- resources/views/components/dashboard/footer.blade.php
+++ resources/views/components/dashboard/footer.blade.php
@@ -16,6 +16,9 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
 <script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+
+{{ $page ?? $page = "" }}
+
 @if ($page === "dashboard")
 
 <script src="{{ $chart->cdn() }}"></script>
