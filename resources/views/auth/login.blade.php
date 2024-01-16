fc3c2b06bff8b12bb62f473bc99dcd1080595618
hilmanXcode <hilmanxcode@gmail.com>
2024-01-16 10:30:35 +0700

    Fix login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,9 @@
     <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
         data-sidebar-position="fixed" data-header-position="fixed">
         <div
-            class="position-relative overflow-hidden min-vh-100 d-flex align-items-center justify-content-center">
-            <div class="d-flex align-items-center justify-content-center w-100">
-                <div class="row justify-content-center w-100 mx-auto rounded-lg">
+            class="d-flex align-items-center justify-content-center">
+            <div class="d-flex w-100 min-vh-100 pb-5">
+                <div class="row justify-content-center my-auto w-100 mx-auto rounded-lg">
                     <div class="col-md-7 col-lg-6 col-xxl-3">
                         <div class="card mb-0 mx-auto rounded-lg shadow">
                             <div class="card-body">
